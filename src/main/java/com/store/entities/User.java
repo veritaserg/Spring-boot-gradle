0c9aeeb562493c5cbb927ec5eed16f165baec2d3
sergey.sergeev <sergey.sergeev@quickblox.com>
2019-07-09 10:25:13 +0300

    create user

--- src/main/java/com/store/entities/User.java
+++ src/main/java/com/store/entities/User.java
@@ -1,15 +1,10 @@
 package com.store.entities;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Builder
 public class User {
 
     private Long id;
     private String name;
     private String password;
+
+
 }
