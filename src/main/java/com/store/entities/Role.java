45cf7aa04a6dca658a0c7b6c629b28336ee52023
sergey.sergeev <sergey.sergeev@quickblox.com>
2019-07-15 12:37:27 +0300

    test build.gradle

---
+++ src/main/java/com/store/entities/Role.java
@@ -0,0 +1,34 @@
+package com.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
